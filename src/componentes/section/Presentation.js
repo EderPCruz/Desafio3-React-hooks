c9efef42f3ff8c52e2066017094867a3b86c9d5c
EderPCruz <ederpaulinodacruz@gmail.com>
2024-05-10 22:59:21 -0300

    10/05/2024-22:59

--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -3,7 +3,7 @@ import style from './Presentation.module.css';
 function Presentation () {
     return(
         <div id='Presentations' className={style.presentation}>
-            <h1>Presentation1</h1>
+            <h1>Presentation</h1>
         </div>
     )
 }
