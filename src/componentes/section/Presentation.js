dc874f884b69324e6bc118b1181f9da8505b6cec
EderPCruz <ederpaulinodacruz@gmail.com>
2024-05-11 22:06:15 -0300

    11/maio/2024-22:05

--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -3,7 +3,13 @@ import style from './Presentation.module.css';
 function Presentation () {
     return(
         <div id='Presentations' className={style.presentation}>
-            <h1>Presentation</h1>
+            <h4><strong>Bem-vindo ao meu Portfólio</strong></h4>
+            <h1>Olá, eu sou Juan</h1>
+            <p>Sou um apaixonado por tecnologia e soluções inovadoras.Como <br/>
+               Product Manager, eu tenho o compromisso de resolver <br/>
+               problemascomplexos e trazer resultados excepcionais para os <br/>
+               negócios.Meus projetos já geraram milhões de reais em receita <br/>
+               anualestou sempre em busca de novos desafios para superar.</p>
         </div>
     )
 }
