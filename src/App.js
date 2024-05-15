aa40aaf253692ca44b161c07fef6825fff3ccb42
EderPCruz <ederpaulinodacruz@gmail.com>
2024-05-14 22:34:30 -0300

    14/maio/2024-22:34

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
     <div className="App">
         <NavBar/>
         <Presentation/>
-        <Project/>
         <Skills/>
+        <Project/>
         <Footer/>
     </div>
   );
