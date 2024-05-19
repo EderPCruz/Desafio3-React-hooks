07a4849f5ae81e2c980c975265f74200c205e171
EderPCruz <ederpaulinodacruz@gmail.com>
2024-05-19 16:26:14 -0300

    19/maio2024-16:26

--- src/componentes/section/Footer.js
+++ src/componentes/section/Footer.js
@@ -1,7 +1,22 @@
+import style from './Footer.module.css'
+import { FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
+
 function Footer () {
     return(
-        <div>
-            <h1>Footer</h1>
+        <div className={style.footer}>
+              <ul>
+                <li>
+                   <a href='https://www.instagram.com/'><FaInstagram size={30}/></a> 
+                </li>
+                <li>
+                <a href='https://github.com/'><FaGithub size={30}/></a>
+                </li>
+                <li>
+                    <a href='https://www.linkedin.com/'><FaLinkedin size={30}/></a>
+                </li>
+            </ul>
+            <p>ederpaulinodacruz@yahoo.com.br</p>
+            <p>Juan Reis © 2024</p>
         </div>
     )
 }
--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -5,7 +5,7 @@ function Presentation () {
     return(
         <div id='Presentations' className={style.presentation}>
             <h4><strong>Bem-vindo ao meu Portfólio</strong></h4>
-            <h1>Olá, eu sou Juan</h1>
+            <h1>Olá, eu sou Éder</h1>
             <p>Sou um apaixonado por tecnologia e soluções inovadoras.Como <br/>
                Product Manager, eu tenho o compromisso de resolver <br/>
                problemascomplexos e trazer resultados excepcionais para os <br/>
